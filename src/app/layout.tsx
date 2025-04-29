18dd4c85fd08304fc5294454a36717bce181fab1
endy-key <yukiendo0408@gmail.com>
2025-04-29 15:08:53 +0900

    move _components from app to src

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Footer } from "./_components/Footer";
-import { Header } from "./_components/Header";
-import { Nav } from "./_components/Nav";
+import { Footer } from "@/components/Footer";
+import { Header } from "@/components/Header";
+import { Nav } from "@/components/Nav";
 import React from "react";
 import styles from "./layout.module.css";
 
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import styles from "./style.module.css";
 
 export function Footer() {
