8503cb60221a313afce9641bf041846ac82f9a51
endy-key <yukiendo0408@gmail.com>
2025-04-30 00:03:32 +0900

    add eyecatch

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,10 +2,10 @@ import styles from "./style.module.css";
 import Image from "next/image";
 
 export function Header() {
-    return (
+     return (
         <header className={styles.header}>
             <div className={styles.logoArea}>
-                <Image src={"/logo.png"} alt={"untilcoffeecools logo"} width={120} height={60} priority/>
+                <Image src={"/eyecatch.jpg"} alt={"untilcoffeecools logo"} width={700} height={200} priority/>
             </div>
         </header>
     );
