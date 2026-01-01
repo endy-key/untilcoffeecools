4d5cc2f86a1b969931613bcebf89266fd079057a
endy-key <yukiendo0408@gmail.com>
2026-01-01 22:54:21 +0900

    20260101

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -11,6 +11,7 @@ export default function Template({ children }: { children: React.ReactNode }) {
             initial={{ opacity: 0, y: 30 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.4, ease: 'easeInOut' }}
+            style={{ opacity: 0 }}
         >
             {children}
         </motion.div>
