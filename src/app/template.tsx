e62288af16967c1944af5298a4ec849a552de314
endy-key <yukiendo0408@gmail.com>
2026-01-01 17:34:02 +0900

    constを利用

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -3,12 +3,13 @@ import { motion } from 'framer-motion'
 import { usePathname } from 'next/navigation'
 
 export default function Template({ children }: { children: React.ReactNode }) {
+    const pathname = usePathname();
+
     return (
         <motion.div
-            key={usePathname()}
+            key={pathname}
             initial={{ opacity: 0, y: 30 }}
             animate={{ opacity: 1, y: 0 }}
-            // exitはこのファイル内では機能しないため削除
             transition={{ duration: 0.4, ease: 'easeInOut' }}
         >
             {children}
